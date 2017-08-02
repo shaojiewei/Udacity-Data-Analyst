343dc29d2a405ac6543b813450d419426f7f68bb
shaojiewei <1205873089@qq.com>
2017-08-02 15:34:07 +0800

    final p5

--- p5/ud120-projects-master/final_project/poi_id.py
+++ p5/ud120-projects-master/final_project/poi_id.py
@@ -282,64 +282,74 @@ tune_params(lr_grid_search, new_features, new_labels, lr_param)
 #features_train, features_test, labels_train, labels_test = \
 #    train_test_split(features, labels, test_size=0.3, random_state=42)
 
-# from sklearn.cross_validation import StratifiedShuffleSplit
-# def test_classifier(clf, dataset, feature_list, folds = 1000):
-#     data = featureFormat(dataset, feature_list, sort_keys = True)
-#     labels, features = targetFeatureSplit(data)
-#     cv = StratifiedShuffleSplit(labels, folds, random_state = 42)
-#     true_negatives = 0
-#     false_negatives = 0
-#     true_positives = 0
-#     false_positives = 0
-#     for train_idx, test_idx in cv: 
-#         features_train = []
-#         features_test  = []
-#         labels_train   = []
-#         labels_test    = []
-#         for ii in train_idx:
-#             features_train.append( features[ii] )
-#             labels_train.append( labels[ii] )
-#         for jj in test_idx:
-#             features_test.append( features[jj] )
-#             labels_test.append( labels[jj] )
+PERF_FORMAT_STRING = "\
+\tAccuracy: {:>0.{display_precision}f}\tPrecision: {:>0.{display_precision}f}\t\
+Recall: {:>0.{display_precision}f}\tF1: {:>0.{display_precision}f}\tF2: {:>0.{display_precision}f}"
+RESULTS_FORMAT_STRING = "\tTotal predictions: {:4d}\tTrue positives: {:4d}\tFalse positives: {:4d}\
+\tFalse negatives: {:4d}\tTrue negatives: {:4d}"
+from sklearn.cross_validation import StratifiedShuffleSplit
+def test_classifier(clf, dataset, feature_list, folds = 1000):
+     data = featureFormat(dataset, feature_list, sort_keys = True)
+     labels, features = targetFeatureSplit(data)
+     cv = StratifiedShuffleSplit(labels, folds, random_state = 42)
+     true_negatives = 0
+     false_negatives = 0
+     true_positives = 0
+     false_positives = 0
+     for train_idx, test_idx in cv: 
+         features_train = []
+         features_test  = []
+         labels_train   = []
+         labels_test    = []
+         for ii in train_idx:
+             features_train.append( features[ii] )
+             labels_train.append( labels[ii] )
+         for jj in test_idx:
+             features_test.append( features[jj] )
+             labels_test.append( labels[jj] )
         
-#         ### fit the classifier using training set, and test on test set
-#         clf.fit(features_train, labels_train)
-#         predictions = clf.predict(features_test)
-#         for prediction, truth in zip(predictions, labels_test):
-#             if prediction == 0 and truth == 0:
-#                 true_negatives += 1
-#             elif prediction == 0 and truth == 1:
-#                 false_negatives += 1
-#             elif prediction == 1 and truth == 0:
-#                 false_positives += 1
-#             elif prediction == 1 and truth == 1:
-#                 true_positives += 1
-#             else:
-#                 print ("Warning: Found a predicted label not == 0 or 1.")
-#                 print ("All predictions should take value 0 or 1.")
-#                 print ("Evaluating performance for processed predictions:")
-#                 break
-#     try:
-#         total_predictions = true_negatives + false_negatives + false_positives + true_positives
-#         accuracy = 1.0*(true_positives + true_negatives)/total_predictions
-#         precision = 1.0*true_positives/(true_positives+false_positives)
-#         recall = 1.0*true_positives/(true_positives+false_negatives)
-#         f1 = 2.0 * true_positives/(2*true_positives + false_positives+false_negatives)
-#         f2 = (1+2.0*2.0) * precision*recall/(4*precision + recall)
-#         print (clf)
-#         print (PERF_FORMAT_STRING.format(accuracy, precision, recall, f1, f2, display_precision = 5))
-#         print (RESULTS_FORMAT_STRING.format(total_predictions, true_positives, false_positives, false_negatives, true_negatives))
-#         print ("")
-#     except:
-#         print ("Got a divide by zero when trying out:", clf)
-#         print ("Precision or recall may be undefined due to a lack of true positive predicitons.")
+         ### fit the classifier using training set, and test on test set
+         clf.fit(features_train, labels_train)
+         predictions = clf.predict(features_test)
+         for prediction, truth in zip(predictions, labels_test):
+             if prediction == 0 and truth == 0:
+                 true_negatives += 1
+             elif prediction == 0 and truth == 1:
+                 false_negatives += 1
+             elif prediction == 1 and truth == 0:
+                 false_positives += 1
+             elif prediction == 1 and truth == 1:
+                 true_positives += 1
+             else:
+                 print ("Warning: Found a predicted label not == 0 or 1.")
+                 print ("All predictions should take value 0 or 1.")
+                 print ("Evaluating performance for processed predictions:")
+                 break
+     try:
+         total_predictions = true_negatives + false_negatives + false_positives + true_positives
+         accuracy = 1.0*(true_positives + true_negatives)/total_predictions
+         precision = 1.0*true_positives/(true_positives+false_positives)
+         recall = 1.0*true_positives/(true_positives+false_negatives)
+         f1 = 2.0 * true_positives/(2*true_positives + false_positives+false_negatives)
+         f2 = (1+2.0*2.0) * precision*recall/(4*precision + recall)
+         print (clf)
+         print (PERF_FORMAT_STRING.format(accuracy, precision, recall, f1, f2, display_precision = 5))
+         print (RESULTS_FORMAT_STRING.format(total_predictions, true_positives, false_positives, false_negatives, true_negatives))
+         print ("")
+     except:
+         print ("Got a divide by zero when trying out:", clf)
+         print ("Precision or recall may be undefined due to a lack of true positive predicitons.")
 
 
 clf = nb_clf
 features_list = kBest_features
 
-# test_classifier(clf, my_dataset, features_list)
+#test_classifier(clf, my_dataset, features_list)
+#test_classifier(svm_clf, my_dataset, features_list)
+#test_classifier(dt_clf, my_dataset, features_list)
+#test_classifier(rf_clf, my_dataset, features_list)
+#test_classifier(rf_clf, my_dataset, features_list)
+
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
